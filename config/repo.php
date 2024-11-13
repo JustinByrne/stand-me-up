17cdf5ac874015a9fd45cd9e774a2671505ffd51
Justin Byrne <theskaterjesus@gmail.com>
2024-11-13 11:31:29 +0000

    added config and env var for the repo

---
+++ config/repo.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repo URL
+    |--------------------------------------------------------------------------
+    |
+    | This is the url that prepends to the issue number to make it linkable
+    | without the trailing slash
+    |
+    */
+
+    'url' => env('REPO_URL'),
+
+];

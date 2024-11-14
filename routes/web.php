bd2448f680068753a31d9e404d5173f060fc80c4
Justin Byrne <theskaterjesus@gmail.com>
2024-11-14 21:36:33 +0000

    added new routes and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\ClockifyController;
 use App\Http\Controllers\IdsController;
+use App\Http\Middleware\ClockifyNotProvided;
+use App\Http\Middleware\EnsureClockifyIsProvided;
 use App\Livewire\TimeEntryList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', TimeEntryList::class);
-Route::get('/ids', IdsController::class);
+Route::middleware(EnsureClockifyIsProvided::class)
+    ->group(function () {
+        Route::get('/', TimeEntryList::class);
+
+        Route::get('/ids', [IdsController::class, 'index']);
+        Route::post('/ids', [IdsController::class, 'setIds']);
+    });
+
+Route::middleware(ClockifyNotProvided::class)
+    ->group(function () {
+        Route::get('/missing-api-key', [ClockifyController::class, 'edit'])->name('missing-token');
+        Route::post('/missing-api-key', [ClockifyController::class, 'update']);
+    });
